8de9e0d7785b7913b68297fe497097fe544af8c5
Даниил Комаров <danik1600@gmail.com>
2023-02-08 18:13:54 +0300

    Сделал js. Осталась последняя форма

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 // БУРГЕР **********************************************************************
-
 const btnBurger = document.querySelector('#burger');
 const body = document.querySelector('.body');
-btnBurger.addEventListener('click', function() {
+btnBurger.addEventListener('click', function () {
     body.classList.toggle('menu-open');
 });
-
-
-
 
 // НАВИГАЦИЯ**********************************************************************
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
@@ -23,5 +19,29 @@ for (let smoothLink of smoothLinks) {
     });
 };
 
+//ФОРМА 1 - МОДАЛЬНОЕ ОКНО***********************************************************
+const promoBtn = document.querySelector('#promo-btn');
+const promoModal = document.querySelector('#modal__promo');
+const promoForm = document.querySelector('#promo-form');
+const btnClose = document.querySelector('#modal-btn-close');
 
+promoForm.addEventListener('click', function (event) {
+    event.preventDefault();
+});
 
+promoBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    promoModal.classList.remove('hidden');
+    
+    btnClose.addEventListener('click', function () {
+        promoModal.classList.add('hidden');
+    })
+});
+
+promoModal.addEventListener('click', function () {
+    promoModal.classList.add('hidden');
+});
+
+promoModal.querySelector('.modal__window').addEventListener('click', function(event){
+   event.stopPropagation();
+});
